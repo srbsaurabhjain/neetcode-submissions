9cdef42d59461ee6b360d7eccaac6ccc5b17d6ca
srbsaurabhjain <65376499+srbsaurabhjain@users.noreply.github.com>
2026-05-30 00:37:57 +1000

    Add: remove-node-from-end-of-linked-list - submission-1

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.kt
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+        var size = 0
+        var temp1 = head
+        while(temp1!=null){
+            temp1=temp1.next
+            size++
+        }        
+        
+        println(size)
+
+        var remove = size-n
+        var count=1
+        var temp = head
+        if(remove==0){
+            return head?.next
+        }
+        while(temp!=null){
+            println("$count, ${temp.`val`}")
+            if(count==remove){
+                temp.next = temp.next?.next
+            } else {
+                temp = temp.next
+            }
+            count++
+        }
+        return head
+    }
+}
